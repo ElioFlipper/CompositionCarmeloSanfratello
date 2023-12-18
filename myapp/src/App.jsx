f052f4125504e8b94c79eb7ffa8a3860123ecb8e
Elio <elio.sanfratello@gmail.com>
2023-12-18 11:10:05 +0100

    Sbagliai

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { AlertClock } from "./AlertClock";
 import { Color } from "./Color";
 import { Colors } from "./Colors";
@@ -8,13 +9,25 @@ import { ToDoList } from "./ListsAndState";
 import { ToDo } from "./ToDo";
 import { ToDo2 } from "./ToDoProva";
 import { Welcome } from "./Welcome";
-
+import { LanguageContext } from "./assets/LanguageContext";
 
 export function App() {
+
+  const [language, setLanguage] = useState('en')
+  function handleLanguage(language) {
+    setLanguage(language)
+  }
+
   return (
-    <Container title = {<h1>La mia nuova app</h1>}>
+    <Container title={<h1>La mia nuova app</h1>}>
       <Welcome name="Elio" eta={28} />
-      <AlertClock/>
+      <LanguageContext.Provider value={language}>
+        <select name="Language" id="Language" onClick={()=>handleLanguage('it')}>Seleziona lingua
+          <option value="it">Italiano</option>
+          <option value="en" onClick={()=>handleLanguage('en')}>English</option>
+        </select>
+        <AlertClock />
+      </LanguageContext.Provider>
       <br />
       <ToDo2 />
 
